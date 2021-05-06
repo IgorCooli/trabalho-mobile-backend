ca9dfb581775af8be10c1e3d2b5db263c2988a98
iobeica <nova@2021>
2021-05-06 01:20:45 -0300

    Habilitando FeignClient no projeto e importando dependencias

--- src/main/java/br/com/cesjf/trabalhomobile/TrabalhoMobileBackendApplication.java
+++ src/main/java/br/com/cesjf/trabalhomobile/TrabalhoMobileBackendApplication.java
@@ -2,8 +2,10 @@ package br.com.cesjf.trabalhomobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TrabalhoMobileBackendApplication {
 
 	public static void main(String[] args) {
