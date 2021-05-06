671341d79122afcb92c0c1310cdd41416d0ccf5d
iobeica <nova@2021>
2021-05-05 23:33:23 -0300

    Initial commit

---
+++ src/main/java/br/com/cesjf/trabalhomobile/TrabalhoMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesjf.trabalhomobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoMobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoMobileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesjf/trabalhomobile/TrabalhoMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesjf.trabalhomobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
